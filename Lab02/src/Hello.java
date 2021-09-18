512e179f9659fed56db8ecbe34c0a9776543c711
rodrigolara <rodrigolara@localhost>
2021-09-17 17:36:51 -0700

    done

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -7,7 +7,7 @@ public class Hello {
 		// Add a new line with your information
 		System.out.println("302711370 - This line was added by Michael Tang.");
 		System.out.println("220219104 - This line was added by Aaron Than.");
-		System.out.println("218788389 - Dalen Hurtt.");
+		System.out.println("218788389 - This line was added by Dalen Hurtt.");
 		System.out.println("301246361 - This line was added by James Ayers.");
 		System.out.println("218915646 - This line was added by Rodrigo Lara.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
