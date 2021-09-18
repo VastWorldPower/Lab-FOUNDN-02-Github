0711261e7bcd94d52acd80ed217ad2d7d58cd36a
Aaron <Aaron@10.0.0.74>
2021-09-17 17:18:16 -0700

    Aaron Than

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -6,7 +6,7 @@ public class Hello {
 		System.out.println("yang - This line was added by Dr. Yang.");
 		// Add a new line with your information
 		System.out.println("302711370 - This line was added by Michael Tang.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("220219104 - This line was added by Aaron Than.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
