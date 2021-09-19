75d1230b41620d11d6dadc053275dfd0ce4cec9c
Peter <Peter@KERBECS>
2021-09-18 18:06:25 -0700

    Peter Metz

--- Lab02/src/Hello.java
+++ Lab02/src/Hello.java
@@ -11,7 +11,7 @@ public class Hello {
 		System.out.println("301246361 - This line was added by James Ayers.");
 		System.out.println("218915646 - This line was added by Rodrigo Lara.");
 		System.out.println("301367287 - This line was added by Jeb Chua.");
-		System.out.println("SacStateID - This line was added by First name Last name.");
+		System.out.println("301208583 - This line was added by Peter Metz.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
 		System.out.println("SacStateID - This line was added by First name Last name.");
